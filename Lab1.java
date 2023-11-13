4ec6d1d7255d300ff3278e0760ee992717f189d9
phubadinee <phubadine.m@kkumail.com>
2023-11-13 15:45:02 +0700

    add hello world

--- Lab1.java
+++
@@ -1,7 +0,0 @@
-public class Lab1 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Java");
-        System.out.println("python");
-    }
-}
