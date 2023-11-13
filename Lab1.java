0193ce9fbc19b709617d5779a2b8d85a445940b8
phubadinee <phubadine.m@kkumail.com>
2023-11-13 15:08:49 +0700

    add python

--- Lab1.java
+++ Lab1.java
@@ -2,6 +2,6 @@ public class Lab1 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Java");
-        
+        System.out.println("python");
     }
 }
