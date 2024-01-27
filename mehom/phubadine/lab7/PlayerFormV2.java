d09e909df51c95e2b5fb98358ef60bf60dcd29f6
phubadinee <phubadine.m@kkumail.com>
2024-01-28 02:17:44 +0700

    update lab7

--- mehom/phubadine/lab7/PlayerFormV2.java
+++ mehom/phubadine/lab7/PlayerFormV2.java
@@ -2,7 +2,7 @@ package mehom.phubadine.lab7;
 
 import javax.swing.*;
 import java.awt.*;
-
+import javax.swing.border.EmptyBorder;
 
 public class PlayerFormV2 extends PlayerFormV1 {
     static JPanel largPanel;
@@ -19,30 +19,45 @@ public class PlayerFormV2 extends PlayerFormV1 {
         typesCombo.addItem("Amateur");
         typesCombo.addItem("Professional");
         typesCombo.setSelectedItem("Amateur");
-        topPanel.setLayout(new GridLayout(6, 2, 5, 5));
+        topPanel.setLayout(new GridLayout(5, 2, 5, 5));
         topPanel.add(playerTypLabel);
         topPanel.add(typesCombo);
-        JLabel noteLabel = new JLabel("Note:");
-        topPanel.add(noteLabel);
+        
     }
 
     @Override
     protected void addComponents(Container contentPane) { 
         
-        JTextArea txtArea = new JTextArea(3, 35);
+        JLabel noteLabel = new JLabel("Note:");
+    
+        // ScrollPane Config
+        // JTextArea txtArea = new JTextArea(3, 35);
+        JTextArea txtArea = new JTextArea(2, 35);
         txtArea.setText("Thailand will face Oman at the Abdullah bin Khalifa Stadium in Doha, Qatar, on Sunday in their second match of the 2023 AFC Asian Cup, Group F.");
         txtArea.setLineWrap(true);
         txtArea.setWrapStyleWord(true);
         JScrollPane scrollPane = new JScrollPane(txtArea);
 
+        JPanel notePanel = new JPanel(new BorderLayout());
+        notePanel.add(noteLabel, BorderLayout.WEST);
+        notePanel.add(scrollPane, BorderLayout.SOUTH);
+        // JPanel notePanel = new JPanel(new GridLayout(2, 1));
+        // notePanel.add(noteLabel);
+        // notePanel.add(scrollPane);
+
         topPanelComponents();
         addLastComponent();
         confirmButtons();
-        
+
+        int margin = 10;
+        topPanel.setBorder(new EmptyBorder(margin, margin, margin, margin));
+        notePanel.setBorder(new EmptyBorder(margin, margin, margin, margin));
+        buttonPanel.setBorder(new EmptyBorder(margin, margin, margin, margin));
+
         // Add Panel to ContentPane
         contentPane.setLayout(new BorderLayout());
         contentPane.add(topPanel, BorderLayout.NORTH);
-        contentPane.add(scrollPane, BorderLayout.CENTER);
+        contentPane.add(notePanel, BorderLayout.CENTER);
         contentPane.add(buttonPanel, BorderLayout.SOUTH); 
     }
 
@@ -61,7 +76,5 @@ public class PlayerFormV2 extends PlayerFormV1 {
                 createAndShowGUI();
             }
         });
-        
     }
-    
 }
