bc05916adc5cff9a1a3ad68071ced1f516626d8f
phubadinee <phubadine.m@kkumail.com>
2024-01-31 23:29:12 +0700

    add file to lab8

--- mehom/phubadine/lab8/PlayerFormV5.java
+++ mehom/phubadine/lab8/PlayerFormV5.java
@@ -20,6 +20,49 @@ public class PlayerFormV5 extends PlayerFormV4 {
     }
 
     @Override
+    protected void addMenus() {
+        // Intialize Menu
+        menuBar = new JMenuBar();     
+        fileMenu = new JMenu("File");
+        configMenu = new JMenu("Config");
+        menuBar.add(fileMenu);
+        menuBar.add(configMenu);
+
+        // File Menu
+        JMenuItem newItem = new JMenuItem("New", newMenuIcon);
+        JMenuItem openItem = new JMenuItem("Open", openMenuIcon);
+        JMenuItem saveItem = new JMenuItem("Save", saveMenuIcon);
+        JSeparator separator = new JSeparator();
+        JMenuItem ExitItem = new JMenuItem("Exit");
+        fileMenu.add(newItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(separator);
+        fileMenu.add(ExitItem);
+
+        // Color Config
+        colorConfig = new JMenu("Color");
+
+        String[] colorObject = {"Red", "Green", "Blue"};
+        for (int range = 0 ; range< colorObject.length; range++) {
+            JMenuItem size = new JMenuItem(colorObject[range]);
+            colorConfig.add(size);
+        }
+
+        // Size Config
+        sizeConfig = new JMenu("Size");
+        for (int range = 0 ; range<3 ; range++) {
+            int intSize = 16 + (4*range);
+            JMenuItem size = new JMenuItem(Integer.toString(intSize));
+            sizeConfig.add(size);
+        }
+        
+        configMenu.add(colorConfig);
+        configMenu.add(sizeConfig);
+        setJMenuBar(menuBar);
+    }
+
+    @Override
     protected void addComponents(Container contentPane) {
         addMenus();
         addTopPanelComponents();
